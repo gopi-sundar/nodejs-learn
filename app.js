3ccfe4e73ede7727eb2d42457d9067757b8ed207
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2017-12-22 08:56:50 -0600

    5 Prototype inheritance and function constructor

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-var person = {
-	firstname: 'John',
-	lastname: 'Doe',
-	greet: function() {
-		console.log('Hello, ' + this.firstname + ' ' + this.lastname);
-	}
+function Person(firstname, lastname) {
+	
+	this.firstname = firstname;
+	this.lastname = lastname;
+	
+}
+
+Person.prototype.greet = function() {
+	console.log('Hello, ' + this.firstname + ' ' + this.lastname);
 };
 
-person.greet();
+var john = new Person('John', 'Doe');
+john.greet();
 
-console.log(person['firstname']);
+var jane = new Person('Jane', 'Doe');
+jane.greet();
+
+console.log(john.__proto__);
+console.log(jane.__proto__);
+console.log(john.__proto__ === jane.__proto__);
